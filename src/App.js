c43bb766f2fd1fdbb73a0426dbafd758be70438d
Zaw Linn Naing <mrzawlinnaing1001@gmail.com>
2022-11-06 17:38:26 +0630

    done Home component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import "./App.scss";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 import Home from "./containers/home";
 import About from "./containers/about";
 import Skills from "./containers/skills";
@@ -7,24 +9,42 @@ import Resume from "./containers/resume";
 import Portfolio from "./containers/portfolio";
 import Contact from "./containers/contact";
 import NavBar from "./components/NavBar";
+import { particleConfig } from "./particlesConfig";
 
 function App() {
+  const particlesInit = async (main) => {
+    await loadFull(main);
+  };
+
+  const location = useLocation();
+  const renderParticlesJSInHomePage = location.pathname === "/";
+
   return (
     <div className="App">
       {/* Particles JS */}
-
+      {renderParticlesJSInHomePage && (
+        <Particles
+          id="particles"
+          options={particleConfig}
+          init={particlesInit}
+        />
+      )}
       {/* NavBar Component */}
-      <NavBar />
+      <div className="App-navbar-wrapper">
+        <NavBar />
+      </div>
       {/* Main Page Content */}
-      <Routes>
-        {/* Create all Routes */}
-        <Route path="/" index element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/resume" element={<Resume />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <div className="App-main-content-wrapper">
+        <Routes>
+          {/* Create all Routes */}
+          <Route path="/" index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
     </div>
   );
 }
--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -1,7 +1,87 @@
 import "./style.scss";
+import {
+  FaQuestion,
+  FaDownload,
+  FaGithub,
+  FaLinkedin,
+  FaFacebook,
+  FaInstagram,
+  FaTwitter,
+} from "react-icons/fa";
+import { Animate } from "react-simple-animate";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="home" id="home">
+      <div className="home-text-wrapper">
+        <h1>
+          Hello, I'm Zaw Linn Naing,
+          <br />
+          Front-end Developer.🤓💬
+        </h1>
+      </div>
+      <Animate
+        play
+        duration={1.5}
+        delay={1}
+        start={{
+          transform: "translateY(600px)",
+        }}
+        end={{
+          transform: "translateX(0)",
+        }}
+      >
+        <div className="contact-me">
+          <div className="contact-me-buttons-wrapper">
+            <button>
+              hire me
+              <FaQuestion size={17} />
+            </button>
+            <a href="https://www.google.com">
+              resume <FaDownload />
+            </a>
+          </div>
+          <div className="contact-me-socials-wrapper">
+            <a
+              href="https://www.github.com/normanlinn"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaGithub size={32} />
+            </a>
+            <a
+              href="https://www.linkedin.com/normanlinn"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaLinkedin size={32} />
+            </a>
+            <a
+              href="https://www.facebook.com/normanlinn"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaFacebook size={32} />
+            </a>
+            <a
+              href="https://www.instagram.com/normanlinn"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaInstagram size={32} />
+            </a>
+            <a
+              href="https://www.twitter.com/normanlinn"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaTwitter size={32} />
+            </a>
+          </div>
+        </div>
+      </Animate>
+    </section>
+  );
 };
 
 export default Home;
