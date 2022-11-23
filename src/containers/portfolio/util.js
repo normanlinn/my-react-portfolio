cf975fe9c0dbf6cd90c9f5f39915d3a9d1fdde98
normanlinn <mrzawlinnaing1001@gmail.com>
2022-11-23 20:27:36 +0630

    use prettier format to format .sccss files, add showcase images and finish portfolio component

---
+++ src/containers/portfolio/util.js
@@ -0,0 +1,53 @@
+import prjImgOne from "../../images/covid-19-tracker-prj.png";
+import prjImgTwo from "../../images/netflix-clone-react-prj.png";
+import prjImgThree from "../../images/covid-19-tracker-prj.png";
+import prjImgFour from "../../images/netflix-clone-react-prj.png";
+import prjImgFive from "../../images/netflix-clone-react-prj.png";
+
+export const portfolioData = [
+  {
+    sectionId: 2,
+    prjName: "covid-19 tracker",
+    prjLink: "",
+    imgURL: prjImgOne,
+  },
+  {
+    sectionId: 2,
+    prjName: "netflix app clone",
+    prjLink: "",
+    imgURL: prjImgTwo,
+  },
+  {
+    sectionId: 2,
+    prjName: "covid-19 tracker",
+    prjLink: "",
+    imgURL: prjImgThree,
+  },
+  {
+    sectionId: 2,
+    prjName: "covid-19 tracker",
+    prjLink: "",
+    imgURL: prjImgFour,
+  },
+  {
+    sectionId: 3,
+    prjName: "design app",
+    prjLink: "",
+    imgURL: prjImgFive,
+  },
+];
+
+export const filterOptions = [
+  {
+    label: "all",
+    id: 1,
+  },
+  {
+    label: "development",
+    id: 2,
+  },
+  {
+    label: "design",
+    id: 3,
+  },
+];
